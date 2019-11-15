1d28d68a7717dfd7197655c88a034390bc6b2ecb
Jessica Michael <pantherjmm@yahoo.com>
2019-11-15 07:56:51 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,8 +1,11 @@
+
+
 # Provided, don't edit
 require 'directors_database'
 
 # A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
 # [3,4,5], [6]] => [1,2,3,4,5,6].
+'''Ruby
 
 def flatten_a_o_a(aoa)
   result = []
@@ -21,7 +24,7 @@ def flatten_a_o_a(aoa)
 end
 
 def movie_with_director_name(director_name, movie_data)
-  { 
+  {
     :title => movie_data[:title],
     :worldwide_gross => movie_data[:worldwide_gross],
     :release_year => movie_data[:release_year],
